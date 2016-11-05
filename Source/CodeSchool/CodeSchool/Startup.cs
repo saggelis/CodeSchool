9cffc5d49253346facb5933e518139bfe0cd62dd
saggelis <sotirisaggelis@gmail.com>
2016-11-05 14:22:13 +0200

    initial commit for visual studio solution

---
+++ Source/CodeSchool/CodeSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeSchool.Startup))]
+namespace CodeSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
